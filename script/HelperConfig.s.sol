ea9b376ea7156a863daa16adb6b8a79bd1d1325d
Rudra Bhaskar <rbrudra9439@gmail.com>
2025-07-08 19:10:31 +0530

    L-24

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -46,6 +46,11 @@ contract HelperConfig is CodeConstants, Script {
             revert HelperConfig__InvalidChainId();
         }
     }
+
+    function getConfig() public returns( NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
         return NetworkConfig({
             entranceFee: 0.01 ether,
