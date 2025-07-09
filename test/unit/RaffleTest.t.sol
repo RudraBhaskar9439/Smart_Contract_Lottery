b7be4b1e5eecb562f98ef8fcbeec07adf1c17d8a
Rudra Bhaskar <rbrudra9439@gmail.com>
2025-07-09 16:02:00 +0530

    L-29_L-30

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -11,17 +11,20 @@ contract Raffletest is Test {
     Raffle public raffle;
     HelperConfig public helperConfig;
 
-
-    //Test user address
-    address public PLAYER = makeAddr("player");
-    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
-
         uint256 entranceFee;
         uint256 interval;
         address vrfCoordinator;
         bytes32 gasLane;
         uint32 callbackGasLimit;
         uint256 subscriptionId;
+
+    //Test user address
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
+    
 
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
@@ -60,6 +63,31 @@ contract Raffletest is Test {
         // Assert
         address playerRecorded = raffle.getPlayer(0);
         assert(playerRecorded == PLAYER);
+    }
+
+    function testEnteringRaffleEmitsEvent() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(PLAYER);
+        // Assert
+        raffle.enterRaffle{value: entranceFee};
+    }
+
+    function testDontAllowPlayersToEnterWhileRaffleIsCalculating() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval +1);
+        vm.roll(block.number +1 );
+        raffle.performUpkeep("");
+
+        // Act // Assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+       
     }
 
 }
